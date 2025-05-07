16a1aa7d31c4e696d04e5fcc04ecf53f8e66e3bf
HaleKim <94584176+HaleKim@users.noreply.github.com>
2025-05-07 19:43:14 +0900

    Update Time.py

--- 김형래/Implementation/Time.py
+++ 김형래/Implementation/Time.py
@@ -1,1 +1,12 @@
+# from datetime import time
+# from datetime import timedelta
+
+# time(0,0,0)
+
 n = int(input())
+
+# for hour in range(n) :
+#     for minute in range(60) :
+#         for second in range(60) :
+#             time[2] += 1
+print(sum(1 for hour in range(n+1) for minute in range(60) for second in range(60) if '3' in str(second) or '3' in str(minute) or '3' in str(hour)))
