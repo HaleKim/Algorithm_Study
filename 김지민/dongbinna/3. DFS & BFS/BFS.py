bf55775f6cbead6208870fa2d7eb1074de6f871a
j-miniee <potatopizza0518@naver.com>
2025-05-26 22:43:50 +0900

    from collections import deque

--- 김지민/dongbinna/3. DFS & BFS/BFS.py
+++ 김지민/dongbinna/3. DFS & BFS/BFS.py
@@ -3,9 +3,8 @@ from collections import deque
 def bfs(graph, start, visited):
     queue = deque([start])
     visited[start] = True
-
     while queue:
-        print(queue)
+        # print(queue)
         v = queue.popleft()
         print(v, end=' ')
         for i in graph[v]:
